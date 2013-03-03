95f719cbe0abc80c16dd49e8dace644eff6304af
vladislavs1321 <13th_merlin@mail.ru>
2013-03-03 23:36:52 +0300

    change login-form style. Add user class

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
+<?php
+    session_start();   
+    
+    if(null !== $_SESSION['auto_error']) {
+        $error = $_SESSION['auto_error'];
+        var_dump($error);
+    } else {
+        echo("session in progress");
+        var_dump($S_SESSION);
+    }
+?>
 <html>
     <head>
         <title></title>
         <link rel='stylesheet' href='style/style.css'/> 
         <link rel='stylesheet' href='style/jquery.pageslide.css'/> 
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script> <script type="text/javascript" src="js/script.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
     </head>
 
     <body>
@@ -49,18 +62,19 @@
             
             <!-- SI modal -->
             <div id="sing-in-modal" style="display: none;">
-                <form method="post">
+                <form id="myform"method="post" action='user/user.php'>
                     <div ><h1>LOG IN</h1></div>
                     <div id="login-box-name" >Username or Email:</div>
                     <div id="login-box-field">
-                        <input name="q" class="form-login" title="Username" value="" size="30" maxlength="2048" placeholder="Username" />
+                        <input name="username" class="form-login" title="Username" value="" size="30" maxlength="2048" placeholder="Username" />
                     </div>
-
                     <div id="login-box-name">Password:</div>
-                    <div id="login-box-field"><input name="q" type="password" class="form-login" title="Password" value="" size="30" maxlength="2048" placeholder="**********"/>
+                    <div id="login-box-field">
+                        <input name="password" type="password" class="form-login" title="Password" value="" size="30" maxlength="2048" placeholder="**********"/>
                     </div>
                     <div id="login-box-name">
-                        <a class="btn" href="javascript:$.pageslide.close()">Enter</a>
+                           <input type="submit" name="q" class="form-login" value="Enter"/>
+                           
                     </div>
                 </form>
             </div>
@@ -80,8 +94,8 @@
 
 
 <?php
-require_once 'Database.php';
-require_once 'Flow.php';
+    require_once 'Database.php';
+    require_once 'Flow.php';
 
 
 //$f = new Flow(0.3e-6, 0.9e-6, 0, 0.1, 0.4,  0.0000000028, 100000, 0.01);
@@ -89,4 +103,5 @@ require_once 'Flow.php';
 //var_dump($f);
 //var_dump($f->simu2());
 //var_dump(rand(1,10000)*0.0001);
+session_destroy();  
 ?>
--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+$(document).ready(function() {
 
-
+    $('#main-menu').submit(function() {
+        alert('Handler for .submit() called.');
+        return false;
+    });
+    
+});
