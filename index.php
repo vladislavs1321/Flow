d1deabc11ff9739260eeb9c14213855438e4cb74
vladislav1321 <13th_merlin@mail.ru>
2013-04-22 16:13:09 +0300

    prepearing for discr

--- index.php
+++ index.php
@@ -33,69 +33,34 @@ if (is_object($user)){
     </head>
 
     <body>
-            <div class="header">
-                <span>Flow Generation</span>
-            </div>
-            
-            <div class="new-content">
-                
-                <div class="block-1">
-                    <div class="main-menu">
-                        <div id="start" class="option-button passive">START</div>
-                        <div id ="history" class="option-button active">HISTORY</div>
-                    </div>
-                </div>
-                
-                <div class="block-2">
-<!--                    <div class="vars" hidden="false">
-                        <form id="form1" class="varaibles" method="get" action="user/generateFlow.php" >
-                            <ul class="input-data">
-                                <div>
-                                    <div class="varaibles-header">
-                                        <fieldset>
-                                            <legend><span>&nbsp;Generation Method&nbsp;</span></legend>
-                                            <ul class="chkbx">
-                                                <li>
-                                                    <input id="Ffactor" type="checkbox" name="f1" value="f1"><span>Focus factor</span>
-                                                    <input id="T" type="checkbox" name="f2" value="f2"><span>Triplet</span>
-                                                </li>
-                                            </ul>
-                                        </fieldset>
-                                    </div>
-                                </div>
-                                <fieldset>
-                                    <legend><span class="varaibles-header">&nbsp;Input Data&nbsp;</span></legend>
-                                </fieldset>
-                                <li><input id="w0"type="text" name="w0" value="" placeholder="w0"><span>in metres</span>
-                                <li><input id="z0" type="text" name="z0" value="" placeholder="z0"><span>in metres</span>
-                                <li><input id="startTime" type="text" name="startTime" value="" placeholder="Start time"><span>in seconds</span>
-                                <li><input id="endTime" type="text" name="endTime" value="" placeholder="End time"><span>in seconds</span>
-                                <li><input id="diffusion" type="text" name="diffusion" value="" placeholder="Molecules diffusion"><span>metres<sup>2</sup>/seconds</span>
-                                <li><input id="Brightness" type="text" name="Brightness" value="" placeholder="Brightness"><span>in Hz</span>
-                                <li><input id="Neff" type="text" name="Neff" value="" placeholder="Molecules in a volume"><span>pcs</span>
-                            </ul>
-                            <input id="generate" type="button" form="form1" class="btn" value="<<GENERATE>>" disabled="true" style="display: none">
-                        </form>
-                    </div>    -->
-<!--                <div id="history" class="history">
-                        <ul></ul>
-                    </div>-->
-                       
-                </div>
-                <div class="block-3">
-                    <div class="login">
-                        <span>
-                            <span class="username"><?php echo(isset($username) ? $username : ""); ?></span>
-                            <?php if(!isset($username)):?>
-                            <img class="show-pageslider" data-open-slide="login" src="images/login.png" title="Log In"/>
-                            <img class="show-pageslider" data-open-slide="register" src="images/sign_up.png" title="Sign Up"/>
-                            <?php else:?>
-                            <a href='http://flow.local/user/resetCookie.php'><img style='cursor: pointer;margin-left: 5px;' class='logout' src='images/logout.png' title='Log Out'/></a>
-                            <?php endif;?>
-                        </span>
-                    </div>
+        <div class="header">
+            <span>Flow Generation</span>
+        </div>
+
+        <div class="new-content">
+
+            <div class="block-1">
+                <div class="main-menu">
+                    <div id="start" class="option-button passive">START</div>
+                    <div id ="history" class="option-button active">HISTORY</div>
                 </div>
             </div>
+
+            <div class="block-2"></div>
+            <div class="block-3">
+                <div class="login">
+                    <span>
+                        <span class="username"><?php echo(isset($username) ? $username : ""); ?></span>
+                        <?php if(!isset($username)):?>
+                        <img class="show-pageslider" data-open-slide="login" src="images/login.png" title="Log In"/>
+                        <img class="show-pageslider" data-open-slide="register" src="images/sign_up.png" title="Sign Up"/>
+                        <?php else:?>
+                        <a href='http://flow.local/user/resetCookie.php'><img style='cursor: pointer;margin-left: 5px;' class='logout' src='images/logout.png' title='Log Out'/></a>
+                        <?php endif;?>
+                    </span>
+                </div>
+            </div>
+        </div>
         <div id="modal">
 <!--                 SI modal login -->
                 <div data-slider="login" class="sub-modal" style="display: none;">
